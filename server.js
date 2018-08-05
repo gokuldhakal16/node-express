d9554cd6068e859cb44911aaced5e16005337642
gokuldhakal16 <signgokul16@gmail.com>
2018-08-04 21:25:06 -0400

    added: model added

--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+// Mongoose Database Connection
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+db.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', (err) => {
+    console.log(err);
+});
 
 // Init App
 const app = express()
+
+// bring in Models
+let Article = require('./model/article');
 
 // PORT
 const port = 3000
 
 // Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
 
 // Home Route
-app.get('/',(req, res) =>
-    res.send("Hello world form Node.js & Express project")
+app.get('/', (req, res) =>
+    Article.find({}, (err, articles) => {
+        res.render('index',{
+            articles: articles
+        })
+    })
+);
+
+// Add article Route
+app.get('/articles/add', (req, res) =>
+    res.render('add_articles', {
+        name: 'Article'
+    })
 )
 
 // Start Server
