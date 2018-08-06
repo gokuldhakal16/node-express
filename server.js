81a1c5c05db09d30e7b94984782afedec7dcc6ef
gokuldhakal16 <signgokul16@gmail.com>
2018-08-05 23:46:34 -0400

    Final code committed

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport')
+const config = require('./config/database');
+
 
 // Mongoose Database Connection
-mongoose.connect('mongodb://localhost/nodekb');
+mongoose.connect(config.database,{ useNewUrlParser: true });
 let db = mongoose.connection;
 
 // Check connection
@@ -20,7 +27,8 @@ db.on('error', (err) => {
 const app = express()
 
 // bring in Models
-let Article = require('./model/article');
+let Article = require('./models/article');
+
 
 // PORT
 const port = 3000
@@ -29,21 +37,61 @@ const port = 3000
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
 
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator())
+
+// Passport Config
+require('./config/passport')(passport)
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', function(req, res, next){
+    res.locals.user = req.user || null;
+    next();
+  });
+
 // Home Route
 app.get('/', (req, res) =>
     Article.find({}, (err, articles) => {
-        res.render('index',{
-            articles: articles
-        })
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                articles: articles
+            })
+        }
     })
 );
 
-// Add article Route
-app.get('/articles/add', (req, res) =>
-    res.render('add_articles', {
-        name: 'Article'
-    })
-)
+// Routes Files
+let articles = require('./routes/articles');
+let users = require('./routes/users');
+app.use('/articles', articles);
+app.use('/users', users);
 
 // Start Server
 app.listen(port, () =>
